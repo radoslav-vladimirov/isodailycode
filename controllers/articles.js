94949e20921f778db355d6d2dcec519cfefc8f05
Radoslav Vladimirov <>
2019-12-09 22:54:29 +0200

    Added URL health check

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,4 +1,5 @@
 const chance = require('chance').Chance()
+const axios = require('axios')
 
 const js = require('../data/javascript.json')
 const react = require('../data/react.json')
@@ -9,10 +10,23 @@ const other = require('../data/other.json')
 const availableTopics = [css, js, other, react, sfcc].filter(topic => topic.articles.length)
 const weights = availableTopics.map(topic=> topic.probability)
 
-module.exports = {
-    "getArticle": () => {
-        const allAvailableArticles = chance.weighted(availableTopics, weights).articles
-        const selectedArticleIndex = chance.integer({ min: 0, max: allAvailableArticles.length })
-        return allAvailableArticles[selectedArticleIndex]
+const randomArticle = async () => {
+    const allAvailableArticles = chance.weighted(availableTopics, weights).articles
+    const selectedArticleIndex = chance.integer({ min: 0, max: (allAvailableArticles.length-1) })
+    const selectedArticle = allAvailableArticles[selectedArticleIndex]
+
+    try {
+        const { status } = await axios.head(selectedArticle)
+        if(status >= 200 && status < 300) {
+            return selectedArticle
+        } else {
+            return randomArticle()
+        }
+    } catch (e) {
+        return randomArticle()
     }
+}
+
+module.exports = {
+    "getArticle": randomArticle
 }
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Koa = require('koa')
+const { getArticle } = require('./controllers/articles.js')
+
 const app = new Koa()
 
-const main = ctx => {
-    const { getArticle } = require('./controllers/articles.js')
-    ctx.response.redirect(getArticle())
+const main = async (ctx) => {
+    await getArticle().then((url) => {
+        ctx.response.redirect(url)
+    }).catch((e) => {
+        ctx.response.redirect('https://www.isobar.com/')
+    })
 }
 
 app.use(main)
